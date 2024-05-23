0b16a2af945e6c20756194d07c4b14aadfe10647
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-05-23 23:26:26 +0300

    fix step3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output',)
     args = parser.parse_args()
-    result = generate_diff(args.first_file, args.second_file, args.format)
+    result = generate_diff(args.first_file, args.second_file)
     print(result)
 
 if __name__ == '__main__':
