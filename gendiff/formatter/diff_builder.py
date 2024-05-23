2bf238cc26b33c854b12b865b1b250e616206c9a
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-05-23 20:49:53 +0300

    step3

---
+++ gendiff/formatter/diff_builder.py
@@ -0,0 +1,23 @@
+import json
+
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+
+    diff = []
+
+    for key in sorted(data1.keys() | data2.keys()):
+        if key not in data2:
+            diff.append(f'- {key}: {str(data1[key])}')
+        elif key not in data1:
+            diff.append(f'+ {key}: {data2[key]}')
+        elif key in data1 and data2 and data1[key] != data2[key]:
+            diff.append(f'- {key}: {data1[key]}')
+            diff.append(f'+ {key}: {data2[key]}')
+        else:
+            diff.append(f'  {key}: {data2[key]}')
+
+    stail_diff = "\n ".join(diff)
+
+    return f'{{\n {stail_diff}\n}}'
+
