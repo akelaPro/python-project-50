a85e8aa81a50cf97bf03ef6c06d32dd0b8be9f70
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-06-02 22:22:41 +0300

    fix test_generate_diff

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,10 +2,11 @@ import pytest
 
 from gendiff.diff_builder import generate_diff
 
-@pytest.mark.parametrize("file_path_1, file_path_2, result_file_path", [
+@pytest.mark.parametrize("file_path_1, file_path_2", [
     ('tests/fixtures/file1.json',
-    'tests/fixtures/file2.json', 'tests/fixtures/result.txt')])
+    'tests/fixtures/file2.json')])
 
 
 def test_generate_diff():
-    assert generate_diff(file_path1, file_path2) == result_file_path
+    with open(tests.fixtures.result.txt) as result_file_path:
+        assert generate_diff(file_path1, file_path2) == result_file_path
