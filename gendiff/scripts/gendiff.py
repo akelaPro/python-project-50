9df62c51043f13145b136f8183ebbf97930f091c
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-06-09 20:18:51 +0300

    fix lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,10 @@
 import argparse
 from gendiff.diff_builder import generate_diff
 
+
 def main():
     parser = argparse.ArgumentParser(
-    description='Compares two configuration files and shows a difference.')
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output',)
