12fff2808da04d07e1d15101ec5053f9ae4b7997
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-06-09 20:17:15 +0300

    fix lint

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,6 +1,8 @@
 import json
 
+
 def generate_diff(file_path1, file_path2):
+
     data1 = json.load(open(file_path1))
     data2 = json.load(open(file_path2))
 
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,8 @@ import argparse
 from gendiff.diff_builder import generate_diff
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+    description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output',)
@@ -10,6 +11,6 @@ def main():
     result = generate_diff(args.first_file, args.second_file)
     print(result)
 
+
 if __name__ == '__main__':
     main()
-    
