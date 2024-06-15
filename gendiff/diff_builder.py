dfebbcb6da3d45238bbe0e34f59484aeacdeb9e8
akelaPro <ilya_chernov_2022@mail.ru>
2024-06-15 19:44:47 +0300

    fix diff_builder

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,28 +1,10 @@
-import json
+from gendiff.diff_tree import generate_diff_tree
+
+from gendiff.opener_file import open_file
 
 
 def generate_diff(file_path1, file_path2):
-
-    data1 = json.load(open(file_path1))
-    data2 = json.load(open(file_path2))
-
-    diff = []
-
-    for key in sorted(data1.keys() | data2.keys()):
-        if key not in data2:
-            diff.append(f' - {key}: {to_str(data1[key])}')
-        elif key not in data1:
-            diff.append(f' + {key}: {to_str(data2[key])}')
-        elif key in data1 and data2 and data1[key] != data2[key]:
-            diff.append(f' - {key}: {to_str(data1[key])}')
-            diff.append(f' + {key}: {to_str(data2[key])}')
-        else:
-            diff.append(f'   {key}: {to_str(data2[key])}')
-
-    stail_diff = "\n ".join(diff)
-
-    return f'{{\n {stail_diff}\n}}'
-
-
-def to_str(item):
-    return str(item).lower()
+    file1 = open_file(file_path1)
+    file2 = open_file(file_path2)
+    diff = generate_diff_tree(file1, file2)
+    return diff
