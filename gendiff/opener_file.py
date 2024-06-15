62d358a0a9b457bf94df3ea39ae433859534d61d
akelaPro <ilya_chernov_2022@mail.ru>
2024-06-15 19:25:30 +0300

    fix step5

--- gendiff/opener_file.py
+++ gendiff/opener_file.py
@@ -7,4 +7,4 @@ def open_file(path):
     if path.endswith('yaml') or path.endswitch('yml'):
         return yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
     else:
-        return json.load(open(path))
+        return json.load(open(path, 'r'))
