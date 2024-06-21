c3d0bca8ea71fa793496c39e0de5984d40873023
akelaPro <ilya_chernov_2022@mail.ru>
2024-06-21 19:06:22 +0300

    update dtylish

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -7,9 +7,13 @@ path2_json = 'tests/fixtures/file2.json'
 diff_file = 'tests/fixtures/result.txt'
 path1_yaml = 'tests/fixtures/file1.yaml'
 path2_yaml = 'tests/fixtures/file2.yaml'
-
+path_nested1_json = 'tests/fixtures/nested1.json'
+path_nested2_json = 'tests/fixtures/nested2.json'
+pat_nested_result = 'tests/fixtures/nested_result.txt'
 @pytest.mark.parametrize('path1, path2, diff',
                           [(path1_json, path2_json, diff_file),
-                            (path1_yaml, path2_yaml, diff_file)])
+                            (path1_yaml, path2_yaml, diff_file),
+                            (path_nested1_json, path_nested2_json,
+                             pat_nested_result)])
 def test_generate_diff(path1, path2, diff):
     assert generate_diff(path1, path2) == open(diff).read()
