3412b1ae71d22769d58c03c0a53dfcdfd45fe0c3
akelaPro <ilya_chernov_2022@mail.ru>
2024-06-21 19:13:44 +0300

    update scripts format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.parser import parse
 
 def main():
     args = parse()
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
