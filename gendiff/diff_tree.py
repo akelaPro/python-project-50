0195b45784d08fe9919f2ba39b64ee61fbbe3f5b
akelaPro <ilya_chernov_2022@mail.ru>
2024-06-21 16:51:32 +0300

    fix diff_tree

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -4,38 +4,43 @@ def generate_diff_tree(file1, file2):
     all_keys = keys1 | keys2
     for key in all_keys:
         if key not in keys1:
-            step_res = {
+            step_ressult = {
                 'name': key,
                 'status': 'added',
+                'what_added': file2.get(key)
             }
             result.append(step_res)
         elif key not in keys2:
-            step_res = {
+            step_ressult = {
                 'name': key,
                 'status': 'deleted',
+                'what_deleted': file2.get(key)
             }
             result.append(step_res)
         elif file1.get(key) == file2.get(key):
-            step_res = {
+            step_ressult = {
                 'name': key,
-                'status': 'unchagent',
+                'status': 'unchangent',
+                'intact': file1.get(key)
             }
             result.append(step_res)
         elif isinstance(
             file1.get(key), dict) and isinstance(
                 file2.get(key), dict):
-            step_res = {
+            step_ressult = {
                 'name': key,
                 'status': 'nested',
                 'children': generate_diff_tree(
                     file1.get(key), file2.get(key))
             }
-            result.append(step_res)
+            result.append(step_ressult)
         else:
             step_result = {
                 'name': key,
                 'status': 'changed',
+                'from_first_dict': file1.get(key),
+                'from_second_dict': file2.get(key)
             }
-            result.append(step_res)
+            result.append(step_ressult)
     diff = sorted(result, key=lambda x: x['name'])
     return diff
