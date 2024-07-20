a0f37d1d669ef0ff29d95ef4866e28fa0f336778
akelaPro <ilya_chernov_2022@mail.ru>
2024-07-20 16:42:07 +0300

    fix lint

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -4,13 +4,16 @@ def plain_formatter(diff, path=""):
         status = unit_diff['status']
         name = path + unit_diff['name']
         if status == 'added':
-            result += f"Property '{name}' was added with value: {to_str(unit_diff['what_added'])}\n"
+            result += f"Property '{name}' "
+            result += f"was added with value: {to_str(unit_diff['what_added'])}\n"
 
         elif status == 'deleted':
             result += f"Property '{name}' was removed\n"
 
         elif status == 'changed':
-            result += f"Property '{name}' was updated. From {to_str(unit_diff['from_first_dict'])} to {to_str(unit_diff['from_second_dict'])}\n"
+            result += f"Property '{name}' "
+            result += f"was updated. From {to_str(unit_diff['from_first_dict'])} "
+            result += f"to {to_str(unit_diff['from_second_dict'])}\n"
 
         else:
             nested = plain_formatter(unit_diff.get('children', []), name + ".")
