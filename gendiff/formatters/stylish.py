b70a0ec64bcc631ba21afa21eb0672ef8e017541
akelaPro <ilya_chernov_2022@mail.ru>
2024-07-20 16:31:59 +0300

    fix lint

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -16,7 +16,7 @@ def to_str(key, value, depth):
         return '\n'.join(lines)
     else:
         return f'{key}: {str(value)}'
-   
+
 
 def default_formatter(diff):
     def make_diff(current_diff, depth=1):
@@ -24,21 +24,26 @@ def default_formatter(diff):
         for unit_diff in current_diff:
             status = unit_diff['status']
             if status == 'added':
-                lines.append(DEF * (depth - 1) + ADD + to_str(unit_diff["name"], unit_diff["what_added"], depth))
+                lines.append(DEF * (depth - 1) + ADD + to_str(
+                    unit_diff["name"], unit_diff["what_added"], depth))
 
             elif status == 'deleted':
-                lines.append(DEF * (depth - 1) + DEL + to_str(unit_diff["name"], unit_diff["what_deleted"], depth))
+                lines.append(DEF * (depth - 1) + DEL + to_str(
+                    unit_diff["name"], unit_diff["what_deleted"], depth))
 
             elif status == 'unchangent':
-                lines.append(DEF * (depth) + to_str(unit_diff["name"], unit_diff["intact"], depth))
+                lines.append(DEF * (depth) + to_str(
+                    unit_diff["name"], unit_diff["intact"], depth))
                 
             elif status == 'nested':
                 nested = make_diff(unit_diff['children'], depth + 1)
                 lines.append(f'{DEF * depth}{unit_diff["name"]}: {nested}')
 
             elif status == 'changed':
-                lines.append(DEF * (depth - 1) + DEL + to_str(unit_diff["name"], unit_diff['from_first_dict'], depth))
-                lines.append(DEF * (depth - 1) + ADD + to_str(unit_diff["name"], unit_diff['from_second_dict'], depth))
+                lines.append(DEF * (depth - 1) + DEL + to_str(
+                    unit_diff["name"], unit_diff['from_first_dict'], depth))
+                lines.append(DEF * (depth - 1) + ADD + to_str(
+                    unit_diff["name"], unit_diff['from_second_dict'], depth))
         output = '{\n' + '\n'.join(lines) + f"\n{(DEF * (depth - 1))}}}"
         return output
     return make_diff(diff)
