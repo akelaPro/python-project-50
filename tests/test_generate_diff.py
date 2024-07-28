604b5fa877f3576959f2ec6ae2b1b7e714733d52
akelaPro <ilya_chernov_2022@mail.ru>
2024-07-28 16:37:54 +0300

    fix lint

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -10,27 +10,27 @@ NESTED_FILE = {
 }
 
 PLAIN_FILE = {
-'file1.json': 'tests/fixtures/file1.json',
-'file2.json': 'tests/fixtures/file2.json',
-'file1.yaml': 'tests/fixtures/file1.yaml',
-'file2.yaml': 'tests/fixtures/file2.yaml'
+  'file1.json': 'tests/fixtures/file1.json',
+  'file2.json': 'tests/fixtures/file2.json',
+  'file1.yaml': 'tests/fixtures/file1.yaml',
+  'file2.yaml': 'tests/fixtures/file2.yaml'
 }
 
 RESULT = {
-'plain_result': 'tests/fixtures/plain_result.txt',
-'nested_result': 'tests/fixtures/nested_result.txt',
-'defoult_result': 'tests/fixtures/result.txt',
-'for_json_formatter': 'tests/fixtures/json_formatter_result.txt'
+  'plain_result': 'tests/fixtures/plain_result.txt',
+  'nested_result': 'tests/fixtures/nested_result.txt',
+  'defoult_result': 'tests/fixtures/result.txt',
+  'for_json_formatter': 'tests/fixtures/json_formatter_result.txt'
 }
 
 
 @pytest.mark.parametrize('path1, path2, formatter, diff',
   [(PLAIN_FILE['file1.json'], PLAIN_FILE['file2.json'], 'stylish', RESULT['defoult_result']),
   (PLAIN_FILE['file1.yaml'], PLAIN_FILE['file2.yaml'], 'stylish', RESULT['defoult_result']),
-   (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'stylish', RESULT['nested_result']),
-   (NESTED_FILE['nested1.yaml'], NESTED_FILE['nested2.yaml'], 'stylish', RESULT['nested_result']),
-   (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'plain', RESULT['plain_result']),
-   (NESTED_FILE['nested1.yaml'], NESTED_FILE['nested2.yaml'], 'plain', RESULT['plain_result']),
-   (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'json', RESULT['for_json_formatter'])])
+  (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'stylish', RESULT['nested_result']),
+  (NESTED_FILE['nested1.yaml'], NESTED_FILE['nested2.yaml'], 'stylish', RESULT['nested_result']),
+  (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'plain', RESULT['plain_result']),
+  (NESTED_FILE['nested1.yaml'], NESTED_FILE['nested2.yaml'], 'plain', RESULT['plain_result']),
+  (NESTED_FILE['nested1.json'], NESTED_FILE['nested2.json'], 'json', RESULT['for_json_formatter'])])
 def test_generate_diff(path1, path2, formatter, diff):
   assert generate_diff(path1, path2, formatter) == open(diff).read()
